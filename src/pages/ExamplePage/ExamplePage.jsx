0f387570bd80c7f1f80ada8c90d8d04c2d39e75b
GannaKov <zlatta2000@gmail.com>
2024-02-23 22:47:56 +0100

    example

--- src/pages/ExamplePage/ExamplePage.jsx
+++ src/pages/ExamplePage/ExamplePage.jsx
@@ -1,7 +1,8 @@
+import styles from "./ExamplePage.module.css";
 const ExamplePage = () => {
   return (
     <main>
-      <h1>Welcome</h1>
+      <h1 className={styles.exampleTitle}>Welcome</h1>
       {/* <img src="https://via.placeholder.com/960x240" alt="" /> */}
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
